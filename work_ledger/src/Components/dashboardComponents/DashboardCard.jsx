dd1102ebf22d2bda91257303520ddf9f53cbd996
Rishank10203 <131427045+Rishank10203@users.noreply.github.com>
2026-03-24 21:51:15 +0530

    Delete work_ledger directory

--- work_ledger/src/Components/dashboardComponents/DashboardCard.jsx
+++
@@ -1,8 +0,0 @@
-export default function DashboardCard({ title, value }) {
-  return (
-    <div className="card card-shadow p-3">
-      <h6 className="text-muted">{title}</h6>
-      <h2 className="fw-bold">{value}</h2>
-    </div>
-  );
-}
